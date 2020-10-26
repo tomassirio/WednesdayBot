5d86334d9ebcd03edb50fb69089cc40e291fac17
Jip Sterk <17934897+JipSterk@users.noreply.github.com>
2020-10-26 15:05:59 +0100

    fix bug where wouldn't pick random image

--- src/commands/wednesday.js
+++ src/commands/wednesday.js
@@ -1,42 +1,42 @@
-const Util = require('../utils/utils.js')
+const Util = require("../utils/utils.js");
 
 module.exports = {
-    name: 'wednesday',
-    description: 'Is it Wednesday?',
-    execute: async (message, args) => {
-        const today = new Date()
-        const { channel } = message
-        const color = '0x2e3136'
-        const collection = [
-            'https://i.imgur.com/1Ezcod1.png',
-            'https://i.imgur.com/UHp7TTB.png',
-            'https://i.imgur.com/ZFzE9TI.png'
-        ];
+  name: "wednesday",
+  description: "Is it Wednesday?",
+  execute: async (message, args) => {
+    const today = new Date();
+    const { channel } = message;
+    const color = "0x2e3136";
+    const collection = [
+      "https://i.imgur.com/1Ezcod1.png",
+      "https://i.imgur.com/UHp7TTB.png",
+      "https://i.imgur.com/ZFzE9TI.png",
+    ];
 
-        let embededMessage;
-        if(today.getDay() == 3) {
-            let index = Math.floor(Math.random() * Math.floor(collection.length));
-            embededMessage = {
-                message: 'It is wednesday my dudes',
-                imageURI: collection[index]
-            }
-        } else if(today.getDay() == 2) {
-            embededMessage = {
-                message: 'Brace yourselves, wednesday is coming!',
-                imageURI: 'https://i.imgur.com/WohbbRs.png'
-            }
-        } else if(today.getDay() == 4) {
-            embededMessage = {
-                message: 'It was yesterday. Do you want more wednesdays? Me too',
-                imageURI: 'https://i.imgur.com/1lxuqMV.png'
-            }
-        } else {
-            embededMessage = {
-                message: 'It is not :(',
-                imageURI: 'https://i.imgur.com/ais8zgq.jpeg'
-            }
-        }
+    let embededMessage;
+    if (today.getDay() == 3) {
+      let index = Math.floor(Math.random() * collection.length);
+      embededMessage = {
+        message: "It is wednesday my dudes",
+        imageURI: collection[index],
+      };
+    } else if (today.getDay() == 2) {
+      embededMessage = {
+        message: "Brace yourselves, wednesday is coming!",
+        imageURI: "https://i.imgur.com/WohbbRs.png",
+      };
+    } else if (today.getDay() == 4) {
+      embededMessage = {
+        message: "It was yesterday. Do you want more wednesdays? Me too",
+        imageURI: "https://i.imgur.com/1lxuqMV.png",
+      };
+    } else {
+      embededMessage = {
+        message: "It is not :(",
+        imageURI: "https://i.imgur.com/ais8zgq.jpeg",
+      };
+    }
 
-        channel.send(Util.embedMessage(embededMessage, color))
-    },
-}
+    channel.send(Util.embedMessage(embededMessage, color));
+  },
+};
